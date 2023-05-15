a7b70a3168d0fb40306911460c3369c57940b4a8
Mathieu Gabelle <mathieu.gabelle@adevinta.com>
2023-05-15 15:14:01 -0400

    Modify Background image and cornhole board to counterContainer

--- components/Background.js
+++ components/Background.js
@@ -1,13 +1,13 @@
 export default function Background() {
     return (
-        <img src="https://img.freepik.com/free-photo/green-grass-field-background_1252-1099.jpg" style={{
+        <img src="https://i.pinimg.com/736x/6b/e7/60/6be760f63380adc8fbf1f5d9e18f2889.jpg" style={{
             zIndex: -1,
             position: "absolute",
             width: "100vw",
             height: "100vh",
             left: 0,
             top: 0,
-            opacity: 0.7
+            opacity: 0.5
           }}></img>
     )
 }
