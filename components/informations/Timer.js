b3d9ff23cdcf6bfb5bbb1af79b707a8996d6ace9
Mathieu Gabelle <mathieu.gabelle@adevinta.com>
2023-05-29 18:22:05 -0400

    Add notification when time is zero or less than 1 minute

--- components/informations/Timer.js
+++ components/informations/Timer.js
@@ -1,16 +1,24 @@
 import {useState, useEffect} from 'react';
 import {timeToMinutesSecondes} from '../../utils.js';
 
+import Snackbar from '@mui/material/Snackbar';
+
+
 export default function Timer({time, setTime}) {
     const [isPaused, setIsPaused] = useState(true);
     const [timer, setTimer] = useState(null);
     const [imageSource, setImageSource] = useState(getImageSource(true));
+    const [isNotificationOpen, setOpenNotification] = useState(false);
 
     function createTimer() {
         if (time == 0) return null;
         const interval = setInterval(() => {
             setTime(prevTime => {
-                if (prevTime - 1 == 0) {
+                if (prevTime -1 === 60) { //Show notification when there is less than 1 minute
+                    setOpenNotification(true);
+                }
+                if (prevTime - 1 === 0) {
+                    setOpenNotification(true);
                     clearInterval(interval);
                 }
                 return prevTime - 1;
@@ -32,29 +40,38 @@ export default function Timer({time, setTime}) {
     }, [isPaused])
      
     return (
-        <div style={{
-            display: "flex",
-            alignItems: "center"
-        }}>
-            <img 
-                src={imageSource}
-                onClick={() => {
-                    if (time > 0) {
-                        setIsPaused(!isPaused);
-                        setImageSource(getImageSource(!isPaused));
-                    }
-                }}
-                style={{
-                    cursor:"pointer",
-                    height:"40px", 
-                    width:"40px",
-                    marginLeft: "15px",
-                }}
-            ></img>
-            <span style={{marginLeft:"15px"}}>
-                {time === -1 ? "Temps Illimité" : timeToMinutesSecondes(time)}
-            </span>
-        </div>
+        <>
+            <div style={{
+                display: "flex",
+                alignItems: "center"
+            }}>
+                <img 
+                    src={imageSource}
+                    onClick={() => {
+                        if (time > 0) {
+                            setIsPaused(!isPaused);
+                            setImageSource(getImageSource(!isPaused));
+                        }
+                    }}
+                    style={{
+                        cursor:"pointer",
+                        height:"40px", 
+                        width:"40px",
+                        marginLeft: "15px",
+                    }}
+                ></img>
+                <span style={{marginLeft:"15px"}}>
+                    {time === -1 ? "Temps Illimité" : timeToMinutesSecondes(time)}
+                </span>
+            </div>
+            <Snackbar
+                anchorOrigin={{ vertical:'top', horizontal:'right'}}
+                open={isNotificationOpen}
+                onClose={() => {setOpenNotification(false)}}
+                message={time === 0 ? "Temps écoulé" : "Il reste 1 minute"}
+                autoHideDuration={5000}
+            />
+        </>
     )
 }
 
